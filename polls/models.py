a546f9934e05c62e954ca28de08b6d6fe1fa6b76
flavinho6666 <flavio.urquidi@aluno.faculdadeimpacta.com.br>
2019-05-08 22:39:01 -0300

    first commit

---
+++ polls/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length = 200)
+    date_pub = models.DateTimeField('publication date')
+
+
+class Choices(models.Model):
+    choice_text = models.CharField(max_length = 100)
+    votes = models.IntegerField(default=0)
+    question = models.ForeignKey(Question, on_delete = models.CASCADE )
