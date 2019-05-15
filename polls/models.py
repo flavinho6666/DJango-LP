e35274bbc0c467f38c7f9f686b453171239503ba
Flavio Urquidi <flavio.urquidi@aluno.faculdadeimpacta.com.br>
2019-05-15 17:43:21 -0300

    ate passo 3

--- polls/models.py
+++ polls/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 
+
+
 # Create your models here.
 
 class Question(models.Model):
     question_text = models.CharField(max_length = 200)
     date_pub = models.DateTimeField('publication date')
 
+    def __str__(self):
+        return self.question_text
+
 
 class Choices(models.Model):
     choice_text = models.CharField(max_length = 100)
     votes = models.IntegerField(default=0)
     question = models.ForeignKey(Question, on_delete = models.CASCADE )
+
+    def __str__(self):
+        return self.choice_text 
+
+
+        
